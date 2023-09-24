c44efca46fa1c37967f66adbb6d47ed6cad0dc65
Serkan <serkankabadayiogullari8233@gmail.com>
2023-09-24 18:47:44 +0300

    GİRİLEN SAYININ BASAMAK SAYILARINI BULMA
    GİRDİĞİM SAYININ KAÇ BASAMAKLI OLDUĞUNU BULAN KODU YAZDIM

---
+++ Girilen Sayinin Basamk Sayisini Bulma/c# sorular/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace c__sorular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozitif bir sayı giriniz:");
+            decimal sayi = Convert.ToDecimal(Console.ReadLine());
+
+            int sayac = 0;
+            for (decimal i=sayi; sayi>=10; i--)
+            {
+                sayi /=10;
+                sayac++;
+                if (sayi < 10)
+                    break;
+
+            }
+            sayac++;
+            Console.WriteLine("Bu sayı basamak sayısı.:" + sayac);
+
+            while (sayi>=10)
+            {
+                sayi /=10;
+                sayi--;
+                sayac++;
+                if (sayi < 10)
+                    break;
+            }
+            Console.WriteLine("While ile basamak sayısı: " + sayac);
+            Console.Read();
+        }
+    }
+}
